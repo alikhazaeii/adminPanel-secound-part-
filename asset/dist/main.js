1c9e5697ecf7845ccc6b354b59976f1dc3f7f957
Ali <165763641+alikhazaeii@users.noreply.github.com>
2024-11-22 11:29:47 -0800

    Add files via upload

---
+++ asset/dist/main.js
@@ -0,0 +1,65 @@
+"use strict";
+const search = document.getElementById('search');
+console.log(search);
+const main = document.querySelector('main');
+search.addEventListener('keyup', () => {
+    let searchInput = search.value.toLowerCase();
+    const name = document.querySelectorAll('ul>li:nth-of-type(2)');
+    // console.log(searchInput);
+    name.forEach(val => {
+        var _a;
+        const nameText = (_a = val.textContent) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+        if (nameText && nameText.indexOf(searchInput) >= 0) {
+            if (val.parentElement) {
+                val.parentElement.style.display = 'flex';
+            }
+        }
+        else {
+            if (val.parentElement) {
+                val.parentElement.style.display = 'none';
+            }
+        }
+    });
+});
+// search.addEventListener('keyup', () => {
+//   let searchInput: string = search.value.toLowerCase();
+//   // انتخاب همه li‌ها داخل ul
+//   const name = document.querySelectorAll('ul > li:nth-of-type(2)') as NodeListOf<HTMLElement>;
+//   name.forEach(val => {
+//     const nameText = val.textContent?.toLowerCase();
+//     if (nameText && nameText.indexOf(searchInput) >= 0) {
+//       if (val.parentElement) {
+//         val.parentElement.style.display = 'block'; // نمایش آیتم‌هایی که متن جستجو را شامل می‌شوند
+//       }
+//     } else {
+//       if (val.parentElement) {
+//         val.parentElement.style.display = 'none'; // مخفی کردن آیتم‌هایی که متن جستجو را شامل نمی‌شوند
+//       }
+//     }
+//   });
+// });
+fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
+    method: 'GET',
+})
+    .then(res => {
+    if (res.ok) {
+        return res.json();
+    }
+})
+    .then((form) => {
+    form.map((val) => {
+        let ul = document.createElement('ul');
+        ul.innerHTML = `
+        <li> ${val.id}</li>
+        <li> ${val.name}</li>
+        <li> ${val.lastname}</li>
+        <li> ${val.age}</li>
+        <li> ${val.email}</li>
+        <li> ${val.date}</li>
+      `;
+        main.appendChild(ul);
+    });
+})
+    .catch(error => {
+    console.error('Error:', error);
+});
---
+++ asset/src/main.ts
@@ -0,0 +1,83 @@
+
+const search: HTMLInputElement = document.getElementById('search') as HTMLInputElement;
+console.log(search);
+const main: HTMLElement = document.querySelector('main') as HTMLElement
+interface form {
+  id: string
+  name: string;
+  lastname: string;
+  email: string;
+  age: string;
+  date: string;
+}
+
+search.addEventListener('keyup', () => {
+  let searchInput: string = search.value.toLowerCase()
+ 
+  const name = document.querySelectorAll('ul>li:nth-of-type(2)') as NodeListOf<HTMLElement>;
+
+  // console.log(searchInput);
+
+  name.forEach(val => {
+
+
+    const nameText = val.textContent?.toLowerCase()
+    if (nameText && nameText.indexOf(searchInput) >= 0) {
+      if (val.parentElement) {
+        val.parentElement.style.display = 'flex'
+      }
+    
+    }   else {
+      if(val.parentElement){
+        val.parentElement.style.display='none'
+      }
+    }
+  })
+})
+// search.addEventListener('keyup', () => {
+//   let searchInput: string = search.value.toLowerCase();
+
+//   // انتخاب همه li‌ها داخل ul
+//   const name = document.querySelectorAll('ul > li:nth-of-type(2)') as NodeListOf<HTMLElement>;
+
+//   name.forEach(val => {
+//     const nameText = val.textContent?.toLowerCase();
+
+//     if (nameText && nameText.indexOf(searchInput) >= 0) {
+//       if (val.parentElement) {
+//         val.parentElement.style.display = 'block'; // نمایش آیتم‌هایی که متن جستجو را شامل می‌شوند
+//       }
+//     } else {
+//       if (val.parentElement) {
+//         val.parentElement.style.display = 'none'; // مخفی کردن آیتم‌هایی که متن جستجو را شامل نمی‌شوند
+//       }
+//     }
+//   });
+// });
+
+
+fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
+  method: 'GET',
+})
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((form: form[]) => {
+    form.map((val: form) => {
+      let ul: HTMLElement = document.createElement('ul');
+      ul.innerHTML = `
+        <li> ${val.id}</li>
+        <li> ${val.name}</li>
+        <li> ${val.lastname}</li>
+        <li> ${val.age}</li>
+        <li> ${val.email}</li>
+        <li> ${val.date}</li>
+      `;
+      main.appendChild(ul)
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
