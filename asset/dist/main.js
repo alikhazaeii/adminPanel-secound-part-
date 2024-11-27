a21c2a5a09c5a7c10ce13566fa0fded7de6d5986
Ali <165763641+alikhazaeii@users.noreply.github.com>
2024-11-27 22:38:23 +0330

    Add files via upload

--- asset/dist/main.js
+++ asset/dist/main.js
@@ -4,7 +4,7 @@ console.log(search);
 const main = document.querySelector('main');
 search.addEventListener('keyup', () => {
     let searchInput = search.value.toLowerCase();
-    const name = document.querySelectorAll('ul>li:nth-of-type(2)');
+    const name = document.querySelectorAll('ul>li:nth-of-type(1)');
     // console.log(searchInput);
     name.forEach(val => {
         var _a;
@@ -21,24 +21,7 @@ search.addEventListener('keyup', () => {
         }
     });
 });
-// search.addEventListener('keyup', () => {
-//   let searchInput: string = search.value.toLowerCase();
-//   // انتخاب همه li‌ها داخل ul
-//   const name = document.querySelectorAll('ul > li:nth-of-type(2)') as NodeListOf<HTMLElement>;
-//   name.forEach(val => {
-//     const nameText = val.textContent?.toLowerCase();
-//     if (nameText && nameText.indexOf(searchInput) >= 0) {
-//       if (val.parentElement) {
-//         val.parentElement.style.display = 'block'; // نمایش آیتم‌هایی که متن جستجو را شامل می‌شوند
-//       }
-//     } else {
-//       if (val.parentElement) {
-//         val.parentElement.style.display = 'none'; // مخفی کردن آیتم‌هایی که متن جستجو را شامل نمی‌شوند
-//       }
-//     }
-//   });
-// });
-fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
+fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/', {
     method: 'GET',
 })
     .then(res => {
@@ -50,16 +33,72 @@ fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
     form.map((val) => {
         let ul = document.createElement('ul');
         ul.innerHTML = `
-        <li> ${val.id}</li>
-        <li> ${val.name}</li>
-        <li> ${val.lastname}</li>
-        <li> ${val.age}</li>
+      <figure>
+      <img src="asset/img/avatar.png" alt="">
+      </figure>
+    
+        <li> ${val.id} ${val.name} ${val.lastname}</li>
+        <li> AGE: ${val.age}</li>
         <li> ${val.email}</li>
         <li> ${val.date}</li>
+        <div>
+          <i title="Delete" onclick="Delete(${val.id})" class="bi bi-person-x-fill" id="delete"></i>
+          <i title="Edit" onclick="Edit(${val.id})" class="bi bi-pencil-square" Id="edit"></i>
+        </div>
       `;
         main.appendChild(ul);
+        const del = ul.querySelector('div>i:nth-of-type(1)');
+        // console.log(del);
+        const edit = ul.querySelector('div>i:nth-of-type(2)');
+        // console.log(edit);
+        // console.log(val.id);
     });
 })
     .catch(error => {
     console.error('Error:', error);
 });
+function Edit(i) {
+    console.log(i);
+    if (confirm('Are Sure Change The Name??')) {
+        fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/' + i, {
+            method: 'PUT', // or PATCH
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ name: prompt('Enter New Name') })
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            // handle error
+        })
+            .then(task => {
+            // Do something with updated task
+            alert('Your User Updated');
+            location.reload();
+        }).catch(error => {
+            // handle error
+        });
+    }
+}
+function Delete(i) {
+    if (confirm('Are You Sure Delete This Account?')) {
+        fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/' + i, {
+            method: 'DELETE',
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            // handle error
+        }).then(task => {
+            // Do something with deleted task
+            location.reload();
+        }).catch(error => {
+            // handle error
+        });
+    }
+}
+const exit = document.getElementById('exit');
+exit === null || exit === void 0 ? void 0 : exit.addEventListener('click', Exit);
+function Exit() {
+    alert('Good Luck Sir!');
+    location.href = 'https://alikhazaeii.github.io/signupforAdminPanel-firstPart-/';
+}
--- asset/src/main.ts
+++ asset/src/main.ts
@@ -1,18 +1,9 @@
 
-
 const search: HTMLInputElement = document.getElementById('search') as HTMLInputElement;
-
-
-
 console.log(search);
-
-
 const main: HTMLElement = document.querySelector('main') as HTMLElement
-
 interface form {
-  
   id: string
-  
   name: string;
   lastname: string;
   email: string;
@@ -22,25 +13,23 @@ interface form {
 
 search.addEventListener('keyup', () => {
   let searchInput: string = search.value.toLowerCase()
- 
-  const name = document.querySelectorAll('ul>li:nth-of-type(2)') as NodeListOf<HTMLElement>;
+
+  const name = document.querySelectorAll('ul>li:nth-of-type(1)') as NodeListOf<HTMLElement>;
 
   // console.log(searchInput);
 
   name.forEach(val => {
 
 
-
-    
     const nameText = val.textContent?.toLowerCase()
     if (nameText && nameText.indexOf(searchInput) >= 0) {
       if (val.parentElement) {
         val.parentElement.style.display = 'flex'
       }
-    
-    }   else {
-      if(val.parentElement){
-        val.parentElement.style.display='none'
+
+    } else {
+      if (val.parentElement) {
+        val.parentElement.style.display = 'none'
       }
     }
   })
@@ -48,13 +37,7 @@ search.addEventListener('keyup', () => {
 
 
 
-
-
-
-
-
-
-fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
+fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/', {
   method: 'GET',
 })
   .then(res => {
@@ -66,25 +49,81 @@ fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform', {
     form.map((val: form) => {
       let ul: HTMLElement = document.createElement('ul');
       ul.innerHTML = `
-        <li> ${val.id}</li>
-        <li> ${val.name}</li>
-        <li> ${val.lastname}</li>
-        <li> ${val.age}</li>
+      <figure>
+      <img src="asset/img/avatar.png" alt="">
+      </figure>
+    
+        <li> ${val.id} ${val.name} ${val.lastname}</li>
+        <li> AGE: ${val.age}</li>
         <li> ${val.email}</li>
         <li> ${val.date}</li>
+        <div>
+          <i title="Delete" onclick="Delete(${val.id})" class="bi bi-person-x-fill" id="delete"></i>
+          <i title="Edit" onclick="Edit(${val.id})" class="bi bi-pencil-square" Id="edit"></i>
+        </div>
       `;
       main.appendChild(ul)
+      const del = ul.querySelector('div>i:nth-of-type(1)')
+      // console.log(del);
+      const edit = ul.querySelector('div>i:nth-of-type(2)')
+      // console.log(edit);
+
+      // console.log(val.id);
+
+
     });
-    
   })
-  
   .catch(error => {
-    
     console.error('Error:', error);
   });
 
 
+function Edit(i: any) {
+  console.log(i);
+  if (confirm('Are Sure Change The Name??')) {
+    fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/' + i, {
+      method: 'PUT', // or PATCH
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ name: prompt('Enter New Name') })
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // handle error
+    })
 
+      .then(task => {
+        // Do something with updated task
+        alert('Your User Updated')
+        location.reload()
+      }).catch(error => {
+        // handle error
+      })
+  }
 
+}
 
+function Delete(i: number) {
+  if (confirm('Are You Sure Delete This Account?')) {
+    fetch('https://673fa428a9bc276ec4b93059.mockapi.io/signupform/' + i, {
+      method: 'DELETE',
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      // handle error
+    }).then(task => {
+      // Do something with deleted task
+      location.reload()
+    }).catch(error => {
+      // handle error
+    })
+  }
+}
 
+const exit = document.getElementById('exit')
+exit?.addEventListener('click',Exit)
+function Exit(){
+  alert('Good Luck Sir!')
+  location.href='https://alikhazaeii.github.io/signupforAdminPanel-firstPart-/'
+}
